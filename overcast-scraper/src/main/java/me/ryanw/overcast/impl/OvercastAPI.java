3b916a1ed542165d33893be606bf5b7f698dee1d
ryanw-se <admin@ryan-w.me>
2015-07-26 00:54:42 -0400

    Add connection timeout & user agent

--- overcast-scraper/src/main/java/me/ryanw/overcast/impl/OvercastAPI.java
+++ overcast-scraper/src/main/java/me/ryanw/overcast/impl/OvercastAPI.java
@@ -12,8 +12,12 @@ import java.util.List;
 
 public class OvercastAPI {
 
+    private static String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36";
+    private static int connectionTimeout = 3000;
+
     public static OvercastPlayer getPlayer(String username) throws IOException {
-        Connection.Response response = Jsoup.connect("https://oc.tc/users/" + username).method(Connection.Method.GET).execute();
+        Connection.Response response = Jsoup.connect("https://oc.tc/users/" + username)
+                .method(Connection.Method.GET).userAgent(userAgent).timeout(connectionTimeout).execute();
         if (response.statusCode() != 200) return null;
         return new ParsedPlayer(response.parse());
     }
@@ -21,7 +25,8 @@ public class OvercastAPI {
     public static List<OvercastPlayer> getPlayers(List<String> usernames) throws IOException {
         List<OvercastPlayer> playerList = new ArrayList<OvercastPlayer>();
         for (String username : usernames) {
-            Connection.Response response = Jsoup.connect("https://oc.tc/users/" + username).method(Connection.Method.GET).execute();
+            Connection.Response response = Jsoup.connect("https://oc.tc/users/" + username)
+                    .method(Connection.Method.GET).userAgent(userAgent).timeout(connectionTimeout).execute();
             if (response.statusCode() != 200) break;
             playerList.add(new ParsedPlayer(response.parse()));
         }
